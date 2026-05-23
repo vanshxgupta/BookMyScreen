8798fc394d209c00790258406b90441de8255730
Vansh <vanshgupta987654321@gmail.com>
2026-05-23 08:03:26 +0530

    implement seat layout page with header and footer components

--- bms/bms-frontend/src/App.jsx
+++ bms/bms-frontend/src/App.jsx
@@ -5,21 +5,34 @@ import Home from "./pages/Home.jsx";
 import Movies from "./pages/Movies.jsx";
 import MovieDetails from "./pages/MovieDetails.jsx";
 import Profile from "./pages/Profile.jsx";
+import SeatLayout from "./pages/SeatLayout.jsx";
+import { useMatch } from "react-router-dom";
+import Checkout from "./pages/Checkout.jsx";
 
 function App() {
+
+  // Hide header/footer only on seat layout page
+  const isSeatLayoutPage = useMatch(
+    "/movies/:movieId/:movieName/:state/theatre/:theatreId/show/:showId/seat-layout"
+  );
+
+  const isCheckoutPage= useMatch("/shows/:showId/:state/checkout")
+
   return (
     <>
       <div className="flex flex-col min-h-screen">
-        <Header />
+        {!isSeatLayoutPage && !isCheckoutPage && <Header/>}
         <main className="flex-grow">
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/profile/:id" element={<Profile />} />
             <Route path="/movies" element={<Movies />} />
             <Route path="/movies/:state/:movieName/:movieid/ticket" element={<MovieDetails />} />
+            <Route path="/movies/:movieId/:movieName/:state/theatre/:theatreId/show/:showId/seat-layout" element={<SeatLayout />} />
+            <Route path="/shows/:showId/:state/checkout" element={<Checkout/>} />
           </Routes>
         </main>
-        <Footer />
+        {!isSeatLayoutPage && !isCheckoutPage && <Footer />}
       </div>
     </>
   );
