5fccf63b4212c73902c37d3446bc4deee62a4d6a
Vansh <vanshgupta987654321@gmail.com>
2026-06-21 14:35:51 +0530

    update socket connection URL

--- bms/bms-frontend/src/utils/socket.js
+++ bms/bms-frontend/src/utils/socket.js
@@ -1,4 +1,4 @@
 import { io } from 'socket.io-client';
 
 // create a Socket.IO client connection from your frontend to your backend server.
-export const socket = io('http://localhost:9000');
+export const socket = io('http://13.239.247.231');
